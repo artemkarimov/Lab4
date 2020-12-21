8c9427b17064c73e66fb541bd76987eeaaa1a5cc
Artem Karimov <59018831+artemkarimov@users.noreply.github.com>
2020-12-21 14:03:05 +0200

    Update client.js

--- client.js
+++ client.js
@@ -5,7 +5,7 @@ class Client {
     return fetch(`http://localhost:3000/${endpoint}`).then(response => response.json());
   }
   post(endpoint, data) {
-    return fetch(`http://my-json-server.typicode.com/artemkarimov/Lab4/${endpoint}`, { method: "POST", body: JSON.stringify(data) }).then(response => response.json());
+    return fetch(`https://my-json-server.typicode.com/artemkarimov/Lab4/${endpoint}`, { method: "POST", body: JSON.stringify(data) }).then(response => response.json());
   }
 }
 
